a6f73987bef8a3db7c11ad1d7b9f20cea28631e3
PC <PC@DESKTOP-OQJMRRC>
2024-06-28 06:02:12 +0900

    sidebar db

--- son/src/main/java/com/son/app/member/service/ParentService.java
+++ son/src/main/java/com/son/app/member/service/ParentService.java
@@ -5,4 +5,8 @@ import java.util.List;
 public interface ParentService {
 	//전체조회
 	public List<StudentVO> ParentInfoList();
+
+    public List<StudentVO> getStudentsByParentNumber(int parentNumber);
+
+    public int getParentNumberByEmail(String email);
 }
