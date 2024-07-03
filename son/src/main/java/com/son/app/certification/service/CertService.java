9e64515801ef73f4616549bed2607d29d75a5c64
SHR <SHR>
2024-07-03 17:35:42 +0900

    수요일

--- son/src/main/java/com/son/app/certification/service/CertService.java
+++ son/src/main/java/com/son/app/certification/service/CertService.java
@@ -2,10 +2,13 @@ package com.son.app.certification.service;
 
 import org.springframework.mail.SimpleMailMessage;
 
+import com.son.app.member.service.StudentVO;
+
 public interface CertService {
 	public void createNumber();
 	public SimpleMailMessage createMail(String mail);
 	public int sendEmail(String id);
 	public void sendPhone(String phone, int number);
 	public int emailDoubleCheck(String mail);
+	public StudentVO childMailCheck(String mail);
 }
