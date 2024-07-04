e84507e8f1bbad96e44ad9937b3742bef382cef0
xxonroro <sdhan1360@gmail.com>
2024-07-04 17:52:06 +0900

    ..

---
+++ son/src/main/java/com/son/app/exam/service/QuestionOptionsVO.java
@@ -0,0 +1,13 @@
+package com.son.app.exam.service;
+
+import lombok.Data;
+
+@Data
+public class QuestionOptionsVO {
+
+    private String optionOne;
+    private String optionTwo;
+    private String optionThree;
+    private String optionFour;
+    private String optionFive;
+}
