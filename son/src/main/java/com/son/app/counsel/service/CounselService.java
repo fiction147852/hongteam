5deaa6b537c76241e9d3813d3135754d48162481
khj <fiction147852@gmail.com>
2024-07-04 17:49:26 +0900

    khj 24 07 04 상담 거의완료

--- son/src/main/java/com/son/app/counsel/service/CounselService.java
+++ son/src/main/java/com/son/app/counsel/service/CounselService.java
@@ -1,49 +1,45 @@
 package com.son.app.counsel.service;
 
 import java.util.List;
-import java.util.Map;
 
 public interface CounselService {
-	
-	//상담일정 전체조회
+
+	// 상담일정 전체조회
 	public List<CounselVO> counselList();
-	
-	//상담일정 단건조회 
+
+	// 상담일정 단건조회
 	public CounselVO counselInfo(CounselVO counselVO);
 
-	//등록
+	// 등록
 	public int counselInsert(CounselVO counselVO);
-	
-	//수정
-	public Map<String, Object> counselUpdate(CounselVO counnselVO);
-	
-	//삭제
+
+	// 수정
+	public int counselUpdate(CounselVO counnselVO);
+
+	// 삭제
 	public int counselDelete(int counselNo);
 
-	//상담 달력 조회 
+	// 상담 달력 조회
 	public List<CounselVO> counselCalendar();
 
-	// 페이지 - 주간 상담 시간 메뉴 
+	// 페이지 - 주간 상담 시간 메뉴
 	public List<AdmissionCounselPossibilityVO> counselWeekTimeList();
-	
-	// 처리 - 주간 시간 조율 
-	public 	int counselWeekTimeUpdate(List<AdmissionCounselPossibilityVO> admissionCounselPossibilityList);
 
-	
-	// 페이지 - 일간 상담 시간 메뉴 
+	// 처리 - 주간 시간 조율
+	public int counselWeekTimeUpdate(List<AdmissionCounselPossibilityVO> admissionCounselPossibilityList);
+
+	// 페이지 - 일간 상담 시간 메뉴
 	public List<CounselImpossibilityVO> counselDayTimeList();
-	
-	//처리 - 일간 시간 조율
+
+	// 처리 - 일간 시간 조율
 	public int counselDayTimeUpdate(List<CounselImpossibilityVO> list);
 
-	// 클릭시 불가능 시간 처리 
+	// 클릭시 불가능 시간 처리
 	public List<String> getDatImpList(CounselImpossibilityVO Coun);
 
-	// 클릭시 가능한 시간대 처리 
+	// 클릭시 가능한 시간대 처리
 	public String getdayPos(AdmissionCounselPossibilityVO vo);
 
-	public List<CounselVO> counselImpTime();
+	public List<CounselVO> counselImpTime(CounselVO counselVO);
 
-
-	
 }
