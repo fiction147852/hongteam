b761ab50995f6ca114af834de04641e31839e00c
khj <fiction147852@gmail.com>
2024-07-08 18:08:14 +0900

    khj 07 08

--- son/src/main/java/com/son/app/lecture/service/AdminLectureService.java
+++ son/src/main/java/com/son/app/lecture/service/AdminLectureService.java
@@ -1,11 +1,16 @@
 package com.son.app.lecture.service;
 
 import java.util.List;
-import java.util.Map;
 
 public interface AdminLectureService {
 	public List<LectureVO> adminLectureList();
 	
 	public LectureVO adminLectureInfo(Integer lectureNumber);
+
+	public LectureVO adminLectureInsert(LectureVO lectureVO);
+
+	public List<LectureSubjectVO> adminLectureSubjectList();
+
+	public List<LectureSubjectDetailVO> adminLectureSubjectDetailList();
 
 }
