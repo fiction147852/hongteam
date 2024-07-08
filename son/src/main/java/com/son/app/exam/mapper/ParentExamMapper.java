8b79e8411670837d029402bdd1d005e71c0d1fb5
{kyungminsu98} <admin@YD05-21>
2024-07-08 18:02:59 +0900

    parentExam

--- son/src/main/java/com/son/app/exam/mapper/ParentExamMapper.java
+++ son/src/main/java/com/son/app/exam/mapper/ParentExamMapper.java
@@ -1,5 +1,27 @@
+// ParentExamMapper.java
 package com.son.app.exam.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.son.app.exam.service.ExamInfoVO;
+import com.son.app.exam.service.ExamListVO;
+import com.son.app.exam.service.GradingResult;
+import com.son.app.exam.service.ParentChildExamVO;
 
 public interface ParentExamMapper {
 
+    // 시험 목록
+	public List<ParentChildExamVO> childExamList(Integer studentNumber, String testTitle, String participateStatus,
+			int startRow, int endRow);
+	
+    public int studentExamCount(@Param("lectureNumber") Integer lectureNumber, @Param("testTitle") String testTitle, @Param("participateStatus") String participateStatus);
+
+    // 시험지
+    public List<ExamInfoVO> studentExamInfo(@Param("testNumber") Integer testNumber);
+
+    // 시험 결과
+    public int getExamResults(GradingResult gradingResultList);
+    public int modifyParticipateStatus(Integer participateNumber);
 }
--- son/src/main/java/com/son/app/exam/service/ParentExamService.java
+++ son/src/main/java/com/son/app/exam/service/ParentExamService.java
@@ -1,5 +1,22 @@
 package com.son.app.exam.service;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.son.app.page.PageVO;
+
 public interface ParentExamService {
 
+    // 시험 목록
+    public List<ParentChildExamVO> childexamList(@Param("lectureNumber") Integer lectureNumber, @Param("testTitle") String testTitle, @Param("participateStatus") String participateStatus, @Param("startRow") int startRow, @Param("endRow") int endRow);
+    public int examCount(@Param("lectureNumber") Integer lectureNumber, @Param("testTitle") String testTitle, @Param("participateStatus") String participateStatus);
+
+    // 시험 상세 정보
+    public List<ExamInfoVO> examInfo(@Param("testNumber") Integer testNumber);
+
+    // 시험 결과
+    public void autoGradeExam(GradingResult gradingResultList);
+    public int modifyParticipateStatus(Integer participateNumber);
+	
 }
