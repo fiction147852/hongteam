1ffa527e19d862c3477703ce11f33d38c67d9c09
qwerty729 <158171842+qwerty729@users.noreply.github.com>
2024-07-15 17:52:52 +0900

    task, tasksubmitlist, info, application-linux.properties added

--- son/src/main/java/com/son/app/lecture/online/mapper/InsOnlineLectureMapper.java
+++ son/src/main/java/com/son/app/lecture/online/mapper/InsOnlineLectureMapper.java
@@ -2,12 +2,17 @@ package com.son.app.lecture.online.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.son.app.lecture.online.service.OnlineLectureVO;
 
 public interface InsOnlineLectureMapper {
-	public List<OnlineLectureVO> selectOnlineLectureAll();
+	public List<OnlineLectureVO> selectOnlineLectureAll(@Param("lectureNumber") Integer lectureNumber,
+														@Param("start") int start,
+														@Param("end") int end);
+	int countOnlineLecs(@Param("lectureNumber") Integer lectureNumber);
 	
-	public OnlineLectureVO selectLectureInfo(OnlineLectureVO onlineLectureVO);
+	public OnlineLectureVO selectLectureInfo(@Param("onlineLectureNumber") Integer onlineLectureNumber);
 	
 	public int insertOnlineLectureInfo(OnlineLectureVO onlineLectureVO);
 	
--- son/src/main/java/com/son/app/lecture/online/service/InsOnlineLectureService.java
+++ son/src/main/java/com/son/app/lecture/online/service/InsOnlineLectureService.java
@@ -3,10 +3,15 @@ package com.son.app.lecture.online.service;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.ibatis.annotations.Param;
+
+import com.son.app.page.PageVO;
+
 public interface InsOnlineLectureService {
-	public List<OnlineLectureVO> onlineLectureList();
+	public List<OnlineLectureVO> onlineLectureList(@Param("lectureNumber") Integer lectureNumber, PageVO pageVO);
+	public PageVO getPageInfo(@Param("lectureNumber") Integer lectureNumber, int page);
 	
-	public OnlineLectureVO onlineLecsInfo(OnlineLectureVO onlineLectureVO);
+	public OnlineLectureVO onlineLecsInfo(Integer onlineLectureNumber);
 	
 	public int insertOnlineLecture(OnlineLectureVO onlineLectureVO);
 	
