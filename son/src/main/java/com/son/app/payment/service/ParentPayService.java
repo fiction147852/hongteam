171345d1bb2b0674711fc1af5f463c22d904ab33
{kyungminsu98} <admin@YD05-21>
2024-07-17 18:33:28 +0900

    api

--- son/src/main/java/com/son/app/payment/service/ParentPayService.java
+++ son/src/main/java/com/son/app/payment/service/ParentPayService.java
@@ -5,4 +5,8 @@ import java.util.List;
 public interface ParentPayService {
 	//전체조회
 	public List<ParentPayVO> LecturePayList(Integer parentNumber);
+	
+	public int savePaymentInfo(ParentPayVO paymentInfo);
+    
+    public int updatePaymentStatus(ParentPayVO paymentInfo);
 }
