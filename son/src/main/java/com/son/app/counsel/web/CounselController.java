7d3f59484deccc0f0d9c75a8a82b0661db15b173
khj <fiction147852@gmail.com>
2024-07-17 17:59:13 +0900

    khj 07 17 5:59

--- son/src/main/java/com/son/app/counsel/web/CounselController.java
+++ son/src/main/java/com/son/app/counsel/web/CounselController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -40,7 +41,11 @@ public class CounselController {
 
 	// 상담 스케줄 전체조회
 	@GetMapping("admin/counselList")
-	public String counselList(Model model, Criteria cri) {
+	public String counselList(Model model,
+							  @ModelAttribute("cri") Criteria cri) {
+		if(cri.getPageNum() == null) {
+			cri.setPageNum(1);
+		}
 		List<CounselVO> list = counselService.counselList(cri);
 		model.addAttribute("counselList", list);
 
--- son/src/main/java/com/son/app/member/service/impl/AdminMemberServiceImpl.java
+++ son/src/main/java/com/son/app/member/service/impl/AdminMemberServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.son.app.lecture.service.Criteria;
 import com.son.app.member.mapper.AdminMemberMapper;
 import com.son.app.member.service.AdminMemberService;
 import com.son.app.security.service.MemberVO;
@@ -16,11 +17,18 @@ public class AdminMemberServiceImpl implements AdminMemberService{
 	@Autowired
 	AdminMemberMapper adminMemberMapper;
 	
+	// 정보 조회 리스트 페이징
 	@Override
-	public List<MemberVO> memberList() {
-		return adminMemberMapper.memberSelectAll();
+	public List<MemberVO> memberList(Criteria cri) {
+		return adminMemberMapper.memberSelectAll(cri);
 	}
 
+	// 데이터 갯수
+	@Override
+	public int memListPageing(Criteria cri) {
+		return adminMemberMapper.selectMemberListPageing(cri);
+	}
+	
 	@Override
 	public MemberVO memberInfo(int idNumber, String auth) {
 		MemberVO vo = new MemberVO();
@@ -29,6 +37,8 @@ public class AdminMemberServiceImpl implements AdminMemberService{
 		
 		return adminMemberMapper.selectMemberList(vo);
 	}
+
+
 	
 
 	
